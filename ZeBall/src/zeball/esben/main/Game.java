651f4e3ec5a0a4353771eff472241a38f5433527
Esben <esben@damkjaergaard.com>
2017-09-16 21:46:40 +0200

    Changed the game loop from being handled by a separate thread, to being handled by a JavaFX AnimationTimer object.

--- ZeBall/src/zeball/esben/main/Game.java
+++ ZeBall/src/zeball/esben/main/Game.java
@@ -1,18 +1,51 @@
 package zeball.esben.main;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import zeball.esben.main.object.Ball;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas {
 	
 	private boolean running = false;
-	private Thread game;
 	private static final int WIDTH = 800, HEIGHT = 500;
 	private GraphicsContext g;
 	
 	private Ball ball;
+	
+	
+	private AnimationTimer gameLoop = new AnimationTimer() {
+		//Game Loop Variables
+		private int ticksPerSecond = 60;
+		private double nsPerTick = 1000000000d / ticksPerSecond;
+		
+		private long lastTime = System.nanoTime();
+		
+		private double delta = 0d;
+		
+		private long timer = System.currentTimeMillis();
+		private int ticks = 0;
+		
+		@Override
+		public void handle(long now) {
+			delta += (now-lastTime) / nsPerTick;
+			lastTime = now;			
+			
+			while (delta >= 1) {
+				tick();
+				render();
+				ticks++;
+				delta -= 1;
+			}
+			
+			if (System.currentTimeMillis() - timer >= 1000) {
+				timer += 1000;
+				System.out.println("TICKS: " + ticks);
+				ticks = 0;
+			}
+		}
+	};
 	
 	public Game() {
 		super(WIDTH, HEIGHT);
@@ -21,52 +54,6 @@ public class Game extends Canvas implements Runnable {
 	private void init() {
 		g = getGraphicsContext2D();
 		ball = new Ball(this);
-	}
-	
-	@Override
-	public void run() {
-		
-		init();
-		
-		int ticksPerSecond = 60;
-		double nsPerTick = 1000000000 / ticksPerSecond;
-		
-		long lastTime = System.nanoTime();
-		
-		double delta = 0d;
-		
-		long timer = System.currentTimeMillis();
-		
-		int ticks = 0;
-		
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now-lastTime) / nsPerTick;
-			lastTime = now;			
-			
-			while (delta >= 1) {
-				tick();
-				render();
-				ticks += 1;
-				delta -= 1;
-			}
-			
-			
-			if (System.currentTimeMillis() - timer >= 1000) {
-				timer += 1000;
-				System.out.println("TICKS: " + ticks);
-				ticks = 0;				
-			}
-			
-			try {
-				Thread.sleep(16);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-		stop();
 	}
 	
 	public void tick() {
@@ -89,19 +76,18 @@ public class Game extends Canvas implements Runnable {
 	
 	public void start() {
 		if (running) return;
-		game = new Thread(this, "ZeBall");
-		game.start();
+		
+		init();
+		gameLoop.start();
+		
 		running = true;
 	}
 	
 	public void stop() {
 		if (!running) return;
 		running = false;
-		try {
-			game.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
+		gameLoop.stop();
 	}
 	
 	
